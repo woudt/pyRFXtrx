a4d035e9a2bb08d8883ff810ab414a1513d84504
Daniel <Daniel.H.Iversen@ntnu.no>
2016-02-24 12:53:33 +0100

    Initial tests

---
+++ tests/main.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestRFXTRX(unittest.TestCase):
+    """
+    Tests the server
+    """
+
+    def setUp(self):     # pylint: disable=invalid-name
+        """ things to be run when tests are started. """
+        pass
+
+    def tearDown(self):  # pylint: disable=invalid-name
+        """ Stop down stuff we started. """
+        pass
+
+    def test_server(self):
+        self.assertFalse(False)
+        self.assertEqual(1, 1)
+
