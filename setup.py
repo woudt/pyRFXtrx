25c4311b0bad1319a9715d8faaebc41c8ac8288b
Daniel Hjelseth Høyer <mail@dahoiv.net>
2022-03-01 19:40:58 +0100

    0.28.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name = 'pyRFXtrx',
     packages = ['RFXtrx'],
     install_requires=['pyserial>=2.7'],
-    version = '0.27.1',
+    version = '0.28.0',
     description = 'a library to communicate with the RFXtrx family of devices',
     author='Edwin Woudt',
     author_email='edwin@woudt.nl',
